51c90bf25a0a49ac4871307cfeb8f87354cf54cc
Raghu1505 <60417248+Raghu1505@users.noreply.github.com>
2020-01-29 12:39:52 +0530

    Update pascal_triangle.py

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,12 +1,6 @@
-rows = int(input("Enter the number of rows : "))
-for i in range(0, rows):
-    a = 1
-
-    for j in range(1, rows-i):
-        print("  ", end="")
-    
-    for k in range(0, i+1):
-        print("  ", a, end="")
-        a = int(a * (i - k) / (k + 1))
-
-    print()
+a = [1]
+b = [0]
+n = int(input("Enter the size: "))
+for i in range(n):
+        print(a)
+        a = [j+k for j,k in zip(a+b, b+a)]
